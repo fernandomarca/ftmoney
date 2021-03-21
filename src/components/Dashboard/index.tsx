3636c96fe9b518673fe0733878afa74fd10f236a
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-03-20 23:51:51 -0300

    estrutura da aplicação

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Summary } from "../Summary";
+import { TransationsTable } from "../TransationsTable";
+import { Container } from "./styles";
+export function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+      <TransationsTable />
+    </Container>
+  );
+}
